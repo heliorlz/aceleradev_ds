12892655891fbc807d4291b5956da777cc831925
Helio Alves <heleralvess@gmail.com>
2020-06-16 07:44:40 -0300

    Semana 2: Streamlit - funçoes basica

---
+++ alunos.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+def main():
+    st.title('Hello World')
+    st.header('This is a header')
+    st.subheader('This is a subheader')
+    st.text('This is a text')
+    st.image('logo.png')
+
+
+if __name__ == '__main__':
+    main()
