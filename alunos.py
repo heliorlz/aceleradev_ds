207b6923914898d195248642450fff20c8aee668
Helio Alves <heleralvess@gmail.com>
2020-06-16 08:16:07 -0300

    Semana 2: Streamlit - funçoes de botao,seleçao e file upload

--- alunos.py
+++ alunos.py
@@ -2,11 +2,47 @@ import streamlit as st
 
 
 def main():
-    st.title('Hello World')
-    st.header('This is a header')
-    st.subheader('This is a subheader')
-    st.text('This is a text')
-    st.image('logo.png')
+    st.title("Hello World")
+
+    st.markdown("Botao")
+    botao = st.button("Botao")
+    if botao:
+        st.markdown("Clicado")
+
+    st.markdown("Checkbox")
+    check_box = st.checkbox("Checkbox")
+    if check_box:
+        st.markdown("Checado")
+
+    st.markdown("Radio")
+    radio = st.radio("What's your favorite soccer team?",
+                     ("Corinthians", "Palmeiras", "Sao Paulo"))
+    if radio == "Corinthians":
+        st.markdown("Vai Corinthians")
+    elif radio == "Palmeiras":
+        st.markdown("Porco")
+    else:
+        st.markdown("Bambi")
+
+    st.markdown("Select Box")
+    select_box = st.selectbox("What's your favorite soccer team?",
+                              ("Corinthians", "Palmeiras", "Sao Paulo"))
+    if select_box == "Corinthians":
+        st.markdown("Vai Corinthians")
+    elif select_box == "Palmeiras":
+        st.markdown("Porco")
+    else:
+        st.markdown("Bambi")
+
+    st.markdown("Multi Select")
+    multi_select_box = st.multiselect(
+        "Select your favorites colors:", ["Red", "Black", "Blue", "Yellow"])
+    st.write(f"Your favorite color(s): {multi_select_box}")
+
+    st.markdown("File uploader")
+    file = st.file_uploader("Select your file: ", type="csv")
+    if file is not None:
+        st.markdown("File uploaded")
 
 
 if __name__ == '__main__':
