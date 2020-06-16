08bde549d3a98157719488c92d63f058e79ab60d
Helio Alves <heleralvess@gmail.com>
2020-06-16 08:41:44 -0300

    Semana 2: Streamlit - funçoes para carregamento do Dataframe, slider para mostrar quantidade de registros dinamicamente, algumas visulizaçoes basicas dos dados

--- alunos.py
+++ alunos.py
@@ -1,48 +1,30 @@
 import streamlit as st
+import pandas as pd
 
 
 def main():
-    st.title("Hello World")
+    st.title("AceleraDev Data Science")
+    st.image("logo.png")
 
-    st.markdown("Botao")
-    botao = st.button("Botao")
-    if botao:
-        st.markdown("Clicado")
+    # Carregando DataFrame
+    # Mostrar os registros dinamicamente, de acordo com o valor escolhido no slider
+    slider = st.slider("Values: ", 0, 100)
+    st.markdown("Função Dataframe")
+    df = pd.read_csv("IRIS.csv")
+    st.dataframe(df.head(slider))
 
-    st.markdown("Checkbox")
-    check_box = st.checkbox("Checkbox")
-    if check_box:
-        st.markdown("Checado")
+    st.markdown("Função Table")
+    st.table(df.head(slider))  # Plotando Dataframe de outra forma
 
-    st.markdown("Radio")
-    radio = st.radio("What's your favorite soccer team?",
-                     ("Corinthians", "Palmeiras", "Sao Paulo"))
-    if radio == "Corinthians":
-        st.markdown("Vai Corinthians")
-    elif radio == "Palmeiras":
-        st.markdown("Porco")
-    else:
-        st.markdown("Bambi")
+    st.write(df.columns)
+    # Retorna as medias dos tamanhos das petalas, agrupadas pelas especies
+    st.table(df.groupby("species")["petal_width"].mean())
 
-    st.markdown("Select Box")
-    select_box = st.selectbox("What's your favorite soccer team?",
-                              ("Corinthians", "Palmeiras", "Sao Paulo"))
-    if select_box == "Corinthians":
-        st.markdown("Vai Corinthians")
-    elif select_box == "Palmeiras":
-        st.markdown("Porco")
-    else:
-        st.markdown("Bambi")
-
-    st.markdown("Multi Select")
-    multi_select_box = st.multiselect(
-        "Select your favorites colors:", ["Red", "Black", "Blue", "Yellow"])
-    st.write(f"Your favorite color(s): {multi_select_box}")
-
-    st.markdown("File uploader")
-    file = st.file_uploader("Select your file: ", type="csv")
-    if file is not None:
-        st.markdown("File uploaded")
+    # Carregando DataFrame com o file uploader
+    # file = st.file_uploader("Upload your Dataframe: ", type="csv")
+    # if file is not None:
+    #    df = pd.read_csv(file)
+    #    st.dataframe(df.head())
 
 
 if __name__ == '__main__':
